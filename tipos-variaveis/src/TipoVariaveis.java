824d99bedafe933e34c99933f882e4a8ddf43a2f
jefinhocps <jefr.mo@gmail.com>
2024-05-17 20:36:39 -0300

    Tipos de variaveis

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,17 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+
+        int idade = 34;
+        String nome = "Jeferson";
+        Short numeroShort = 600;
+        // Quando informado o int na frente do numero chamamos de (casting)
+        // Exemplo abaixo 
+        short numeroCurto = (short) idade; 
+        // Definição de Constante, inicia-se pela palavra reservada (final) ou (FINAL), e seguida do nome da variavel em (caixa alta)
+
+        // Exemplo o valor de PI não pode ser alterado
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
