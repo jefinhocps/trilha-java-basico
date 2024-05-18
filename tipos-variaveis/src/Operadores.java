abffb19195fa4a8f666157175a7ff6f888cadab9
jefinhocps <jefr.mo@gmail.com>
2024-05-17 21:04:49 -0300

    expressões

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+        /*  = (atribuição)
+         *  == (igual)
+         *  <=(menor igual)
+         *  >= (maior igual)
+         *  > (maior)
+         *  < (menor)
+         *  + (adicao)
+         *  - (subtracao)
+         *  / (divisao)
+         *  * (multiplicacao)
+         *  % (resto da divisao)
+         */
+
+         int num1 = 5;
+         int num2 = 6;
+
+         // = (atribuição)
+         // num1 = num2;
+         // == (igual)
+         num1 == num2;
+         // <=(menor igual)
+         num1 <= num2;
+         // >= (maior igual)
+         num1 >= num2;
+
+    }
+    
+}
