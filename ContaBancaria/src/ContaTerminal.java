993efd71c347533446290e24edbc224aad660f73
jefinhocps <jefr.mo@gmail.com>
2024-05-18 05:50:43 -0300

    primeiro comit conta banco

--- ContaBancaria/src/ContaTerminal.java
+++ ContaBancaria/src/ContaTerminal.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá seja Bem-vindo(a) ao nosso sistema de Contas.");
+
+        Scanner sc = new Scanner(System.in);
+
+        double Saldo = 237.48;
+
+        System.out.println("Digite o numero da conta: ");
+        int numeroConta = sc.nextInt();
+
+        System.out.println("Informe o numero da agencia: ");
+        String agencia = sc.nextLine();
+        
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        //System.out.println(nome);
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é:" + agencia + " Conta: " + numeroConta + " e seu saldo: " + Saldo + " já esta disponivel para saque.");
+        
 
         
     }
