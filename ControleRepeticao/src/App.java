22ddd41570ed3ee1817e0ef938f96ac82ddd57f1
jefinhocps <jefr.mo@gmail.com>
2024-05-20 00:15:40 -0300

    controle for

---
+++ ControleRepeticao/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Controle FOR
+        for(int x=1; x<=10; x++){
+            System.out.println("Valor: " + x);
+        }
+
+        
+    }
+}
