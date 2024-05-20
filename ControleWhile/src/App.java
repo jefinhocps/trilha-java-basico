629f68ce03ff650d3fb75cfac9fdcc7f673466a0
jefinhocps <jefr.mo@gmail.com>
2024-05-20 03:06:42 -0300

    Implementado a verificação de quantidade de doces no carrinho

--- ControleWhile/src/App.java
+++ ControleWhile/src/App.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         double mesada = 50.0;
+        int qtdDoce = 0;
         
 
         while(mesada > 0){
@@ -10,14 +11,16 @@ public class App {
             Double valorDoce = rand;
 
             if(valorDoce > mesada){
-                valorDoce = mesada;
-
+                mesada = 0;
+            }else {
                 System.out.println("Doce do Valor: " + valorDoce + " adicionado no carrinho.");
                 mesada = mesada - valorDoce;
-            }
-
-            System.out.println("Mesada: " + mesada);
-            System.out.println("Você gastou toda sua mesada...");  
+                qtdDoce++;
+            }  
         }
+
+        System.out.println("Mesada: " + mesada);
+        System.out.println("Você gastou toda sua mesada...");
+        System.out.println("Doces adicionados ao carrinho: " + qtdDoce);
     }   
 }
